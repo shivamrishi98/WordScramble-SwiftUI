f1e4d3dda9ced942dc6bae8f99ff39ff0c73a985
shivamrishi98 <shivamrishisr@gmail.com>
2020-07-15 11:14:30 +0530

    Initial Commit

---
+++ WordsGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordsGame
+//
+//  Created by Shivam Rishi on 15/07/20.
+//  Copyright © 2020 shivam. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
